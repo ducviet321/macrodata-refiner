7d669b13def5115d477df51d4ef2a2870b279246
Vit3060 <ducviet321@gmail.com>
2025-03-29 17:22:38 +0700

    WIP In-Game UI

---
+++ Assests/ui_scaler.gd
@@ -0,0 +1,19 @@
+"""
+https://github.com/godotengine/godot-proposals/discussions/11471#discussioncomment-11762037
+"""
+
+@tool
+extends Container
+
+@export var content_scale := 1.0:
+	set(v):
+		content_scale = v
+		queue_sort()
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_SORT_CHILDREN:
+			for child:Control in get_children():
+				child.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT,Control.PRESET_MODE_MINSIZE)
+				child.size /= content_scale 
+				child.scale = Vector2(content_scale,content_scale)
--- gameplay.gd
+++ gameplay.gd
@@ -1,16 +1,36 @@
 extends CanvasLayer
 
 @onready var input_name: LineEdit = %InputName
+@onready var label_name: Label = %LabelName
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var isGameStarted: bool = false
 
 func _ready() -> void:
 	input_name.text = ""
 	input_name.grab_focus()
+	
+	
+func start():
+	""" Rules:
+	- Find all matched color numbers and fill it in each folder
+		
+	- 5 Folders x 4 Attributes: 
+		WO = Green
+		FC = Yellow
+		DR = White
+		MA = Blue
+	"""
+	isGameStarted = true
 
 
 func _on_input_name_text_submitted(new_text: String) -> void:
+	if isGameStarted: return
+	
 	if len(new_text) < 3:
 		print("New too short")
 		return
+		
+	label_name.text = new_text
+	start()
